e57f33d8d946c1447c97a125d4ec9f16cbf01b6f
JungHoon0814 <kjhoon0814@gmail.com>
2023-01-31 16:10:24 +0900

    Create : 컴포넌트 및 페이지 상세 내용 추가

---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <div>
+              Contact
+            </div>
+            <div>
+              Copyright
+            </div>
+            <div>
+              팀원 @@@
+            </div>
+        </footer>
+    )
+}
---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <button>장바구니</button>
+                <button>로그인</button>
+                <button>회원가입</button>
+            </nav>
+        </header>
+    )
+}
