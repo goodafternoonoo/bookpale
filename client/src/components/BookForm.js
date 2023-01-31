2b33c1eded6b34f70bbb947aa82162f2f36fb2ab
HyeonU-Jo <chw3448@gmail.com>
2023-01-31 20:16:19 +0900

    Update : BookForm 수정 및 reset.css 추가

--- client/src/components/BookForm.js
+++ client/src/components/BookForm.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import png from '../js.png'
+import React from 'react';
+import png from '../js.png';
 
-import '../css/BookForm.css'
+import '../css/BookForm.css';
 
 export default function BookForm() {
     return (
-    <div>
-      <ul className='content'>
-        <li><img alt='' src={png}></img><span>1</span></li>
-        <li><img alt='' src={png}></img><span>2</span></li>
-        <li><img alt='' src={png}></img><span>3</span></li>
-        <li><img alt='' src={png}></img><span>4</span></li>
-        <li><img alt='' src={png}></img><span>5</span></li>
-      </ul>
-    </div>)
+        <div>
+            <div className='content'>
+                <li>
+                    <img alt='' src={png}></img>
+                    <span>제목</span>
+                </li>
+                <li>
+                    <img alt='' src={png}></img>
+                    <span>제목</span>
+                </li>
+                <li>
+                    <img alt='' src={png}></img>
+                    <span>제목</span>
+                </li>
+                <li>
+                    <img alt='' src={png}></img>
+                    <span>제목</span>
+                </li>
+                <li>
+                    <img alt='' src={png}></img>
+                    <span>제목</span>
+                </li>
+            </div>
+        </div>
+    );
 }
