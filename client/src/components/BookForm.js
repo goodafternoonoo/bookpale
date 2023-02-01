f781d73156df82f2033c96173999eb9df32a00ff
JungHoon0814 <kjhoon0814@gmail.com>
2023-02-01 10:55:46 +0900

    Fix : 부트스트랩을 이용한 header footer 커스텀

--- client/src/components/BookForm.js
+++ client/src/components/BookForm.js
@@ -1,33 +1,22 @@
 import React from 'react';
-import png from '../js.png';
+// import png from '../js.png';
+import Card from 'react-bootstrap/Card';
+// import ListGroup from 'react-bootstrap/ListGroup';
 
-import '../css/BookForm.css';
+// import '../css/BookForm.css';
 
 export default function BookForm() {
     return (
-        <div>
-            <div className='content'>
-                <li>
-                    <img alt='' src={png}></img>
-                    <span>제목</span>
-                </li>
-                <li>
-                    <img alt='' src={png}></img>
-                    <span>제목</span>
-                </li>
-                <li>
-                    <img alt='' src={png}></img>
-                    <span>제목</span>
-                </li>
-                <li>
-                    <img alt='' src={png}></img>
-                    <span>제목</span>
-                </li>
-                <li>
-                    <img alt='' src={png}></img>
-                    <span>제목</span>
-                </li>
-            </div>
-        </div>
+       <>
+       <Card style={{ width: '20%' }}>
+      <Card.Img variant="top" src="../js.png" style={{weight: '20px', height: '20px'}}/>
+      <Card.Body>
+        
+        <Card.Text>
+          책이름
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </>
     );
 }
