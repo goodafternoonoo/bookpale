341d2ec8b05f4a3a62ccf59cbbfd2cdd02bf125e
HyeonU-Jo <chw3448@gmail.com>
2023-02-02 19:22:58 +0900

    Add & Fix : db.json, axios 추가 및 메인페이지 로직 변경

--- client/src/components/BookForm.js
+++ client/src/components/BookForm.js
@@ -1,23 +1,37 @@
-import React from 'react';
-// import png from '../js.png';
-import Card from 'react-bootstrap/Card';
-// import ListGroup from 'react-bootstrap/ListGroup';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import { Card, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-// import '../css/BookForm.css';
 import png from '../js.png';
 
 export default function BookForm() {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/books').then((response) => {
+            setBooks(response.data);
+        });
+    }, []);
+
     return (
         <>
-            <Card style={{ width: '20%' }}>
-                <Link to='/detail'>
-                    <Card.Img variant='top' src={png} style={{}} />
-                </Link>
-                <Card.Body>
-                    <Card.Text>책이름</Card.Text>
-                </Card.Body>
-            </Card>
+            <Container className='d-flex my-5 py-5'>
+                {books.map((book) => {
+                    return (
+                        <Card style={{ width: '20%', margin: '0 10px' }}>
+                            <Link to='/detail'>
+                                <Card.Img variant='top' src={png} style={{}} />
+                            </Link>
+                            <Card.Body>
+                                <Card.Title>{book.title}</Card.Title>
+                                <Card.Text>{Number(book.price).toLocaleString()}원</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    );
+                })}
+            </Container>
         </>
     );
 }
