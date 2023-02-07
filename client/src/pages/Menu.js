f1ddc7330504b19919321dd0e0359eabece95188
HyeonU-Jo <chw3448@gmail.com>
2023-02-07 21:55:14 +0900

    Fix : db.js에 .env를 이용한 환경변수 호출

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -41,18 +41,6 @@ export default function Menu() {
                             </span>
                         </div>
                         <div className={styles['menu-body']}>
-                            <p className={(styles['title'], 'text-dark fw-bold fs-4')}>제품판매</p>
-                            <p className={(styles['subtitle'], 'text-dark fs-6')}>제품 정보를 등록하여, 판매를 시작할 수 있습니다.</p>
-                        </div>
-                    </Link>
-                    <Link to='/' className={styles['menu-card']}>
-                        <div className={styles['menu-icon']}>
-                            <span className='icon has-text-info'>
-                                {/* <i className='fa-solid fa-credit-card'></i> */}
-                                <img src={png} width='70' alt='메뉴 아이콘' />
-                            </span>
-                        </div>
-                        <div className={styles['menu-body']}>
                             <p className={(styles['title'], 'text-dark fw-bold fs-4')}>회원탈퇴</p>
                             <p className={(styles['subtitle'], 'text-dark fs-6')}>모든 정보를 안전하게 삭제한 후 탈퇴할 수 있습니다.</p>
                         </div>
--- server/db.js
+++ server/db.js
@@ -1,18 +1,20 @@
 //db 연결 파일
+require('dotenv').config();
 const { ATLAS_NAME, ATLAS_PWD } = process.env;
 const mongoose = require('mongoose');
-mongoose.set('strictQuery',true);
+mongoose.set('strictQuery', true);
 
 module.exports = () => {
-  function connect() {
-    mongoose.connect(
-      `mongodb+srv://${ATLAS_NAME}:${ATLAS_PWD}@atlascluster.vaw0jlk.mongodb.net/?retryWrites=true&w=majority`, function (err) {
-      if (err) {
-        console.error('mongodb connection error', err);
-      }
-      console.log('mongodb connected');
-    });
-  }
-  connect();
-  mongoose.connection.on('disconnected', connect);
+    function connect() {
+        mongoose.connect(`mongodb+srv://${ATLAS_NAME}:${ATLAS_PWD}@atlascluster.vaw0jlk.mongodb.net/?retryWrites=true&w=majority`, function (err) {
+            if (err) {
+                console.error('mongodb connection error', err);
+            } else {
+                console.log('mongodb connected');
+            }
+        });
+    }
+
+    connect();
+    mongoose.connection.on('disconnected', connect);
 };
