e0d8ba8df95d68bd7636a466fe46820e5cad4e59
JungHoon0814 <kjhoon0814@gmail.com>
2023-02-08 22:21:55 +0900

    Delete : SignIn 코드 주석 삭제

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -34,32 +34,6 @@ export default function SignIn() {
             });
     }
 
-    /* 
-    // 페이지를 SPA방식으로 돌아가게 하려는 선언구 = v5버전의 history와 동일
-
-    // 불필요한 컴포넌트의 재렌더링을 막기위해 이 로직을 사용
-    const emailRef = useRef();
-    const passwordRef = useRef();
-    // 에러 핸들링을 하기위해 useState로 선언한 초기값
-    const [errorMessage, setErrorMessage] = useState('');
-
-    // 로그인 입력을 완료했을때 로그인 정보를 넘겨줄 이벤트?
-    const handleSubmit = (event) => {
-        // 이벤트를 명시적으로 처리하지 않은경우 기본동작을 실행하지 않도록하는 코드
-        event.preventDefault();
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
-        emailRef.current.value = '';
-        passwordRef.current.value = '';
-        handleLogin(email, password);
-    };
-
-    // 로그인을 수행시 작동할 통신 코드와 로직
-    const handleLogin = (email, password) => {
-        if (email && password) {
-            axios.post(`http://localhost:3000`);
-        }
-    }; */
 
     return (
         <>
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,19 +1,11 @@
-<<<<<<< HEAD
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = 'blahblah';
 
 const { Router } = require('express');
 const router = Router();
-=======
-import express from 'express';
-import User from '../db/schemas/User.js';
->>>>>>> a6929b9a8380553fcb073c23dc61e0ec99b511f7
 
-// import 구문에서 Route가 무엇을 정의하는지 모름
-// express에서 route를 땡겨오기 위해 사용
-const userRouter = express.Router();
+const User = require('../db/schemas/User.js');
 
-<<<<<<< HEAD
 const { auth } = require('../middleware/authMiddleware.js');
 
 router.get('/', auth, (req, res, next) => {
@@ -31,10 +23,6 @@ router.get('/:id', (req, res, next) => {
 });
 
 router.post('/signup', async (req, res, next) => {
-=======
-
-userRouter.post('/signup', async (req, res, next) => {
->>>>>>> a6929b9a8380553fcb073c23dc61e0ec99b511f7
     // res.send('회원가입 ');
     const { email, password, name, address1, address2, zipCode } = req.body;
     const user = {
@@ -53,7 +41,6 @@ userRouter.post('/signup', async (req, res, next) => {
         .catch((err) => res.status(500).send(err));
 });
 
-<<<<<<< HEAD
 router.post('/login', async (req, res, next) => {
     const { email, password } = req.body;
 
@@ -80,11 +67,6 @@ router.post('/login', async (req, res, next) => {
             });
         })
         .catch((err) => res.status(500).send(err));
-=======
-userRouter.post('/login', async (req, res, next) => {
-    res.send('로그인  ');
->>>>>>> a6929b9a8380553fcb073c23dc61e0ec99b511f7
 });
 
-// module.exports = router;
-export default userRouter;
+module.exports = router;
