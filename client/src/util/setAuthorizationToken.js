ed688898baff58ed832f6a141933d9be25ddb3bb
JungHoon0814 <kjhoon0814@gmail.com>
2023-02-09 19:06:29 +0900

    Fix : 환경변수 변경

--- client/src/util/setAuthorizationToken.js
+++ client/src/util/setAuthorizationToken.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 export default function setAuthorizationToken(token) {
-    axios.defaults.baseURL = 'http://localhost:3000';
+    axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
     if (token) {
         axios.defaults.headers.common['Authorization'] = `${token}`;
     } else {
